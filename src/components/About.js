fe506d26ef1b88a4cbcb2da827df014cda91429d
Anshul <ganshul34@gmail.com>
2019-04-23 23:23:54 +0530

    Added other components

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = ()=>{
+
+    return(
+        <div className="container center">
+Lorem ipsum dolor sit amet, mel antiopam constituam ex. Postea tamquam ne eum, cibo voluptua petentium te nam. Eum libris prompta at, ad eum magna idque blandit. Possim nostrud ius ne.
+
+Qui ne debet perpetua. Graeco prodesset mei ne, id nibh autem postulant vel. Ius at utinam omittantur. Vel no copiosae principes.
+
+Vix te cibo accusamus, ex vel oratio verear. Vix dolore nemore quaestio no, laudem electram ut mei. Mel graeci epicuri incorrupte id, nec nostrum sensibus omittantur eu. In ponderum qualisque vel. Utinam audire adipiscing duo ea, qui maiorum sententiae no. Pri tantas ornatus reprehendunt et.
+
+No nam laboramus constituam, has dictas senserit definitionem ei. Te laudem vocent regione sed, saepe populo aperiam eum ne, te torquatos maiestatis quo. Nec delicata iracundia ut, no antiopam voluptatibus has. Id qui illud intellegat. Ex modus debitis veritus eum, duo amet eirmod ei.
+
+Illud delicatissimi has no, suas vitae recusabo in eam, mazim postea tamquam ut pri. No facilisi argumentum per, at modo ullum sea. Ex mea iudico fabulas. Eum ex harum prompta, vim prompta appareat signiferumque at, vix fugit eripuit contentiones ne. Cu patrioque democritum usu.
+        </div>
+    )
+}
+export default About
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css'
+
+ const Footer = ()=>{
+    return(
+        <footer className="page-footer"  >
+        <div className="footer-copyright">
+          <div className="container">
+          © 2019 All Rights Resurved
+          <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+          </div>
+        </div>
+      </footer>
+        
+    )
+}
+
+export default Footer;
