a1dc86a7402d4895f71738224f4e67beba8a0efd
Anshul <ganshul34@gmail.com>
2019-04-23 23:26:31 +0530

    Added in the main app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Cart from './components/Cart'
+import Footer from './components/Footer';
+import About from './components/About'
+import  NotFound from './components/NotFound';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+       <BrowserRouter>
+            <div className="App">
+              <Navbar/>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/cart" component={Cart}/>
+                    <Route path="/about" component={About}/>
+                    <Route path="*" component={NotFound}/>
+                  </Switch>
+                  <Footer/>
+             </div>
+             
+       </BrowserRouter>
+      
+    );
+  }
 }
 
 export default App;
